4f80c18cff124a7061ae31d81d59b19420246aa5
vstsiganow <slavok-48@mail.ru>
2021-05-29 16:33:37 +0300

    Initial Commit

---
+++ Homework2.4-MiniOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2.4-MiniOrganizer
+//
+//  Created by vtsyganov on 29.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
